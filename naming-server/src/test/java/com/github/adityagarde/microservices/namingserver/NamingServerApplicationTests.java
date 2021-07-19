72971e15dc4ede6ebc08f73f8d664a9f8da8452a
adityagarde <aditya.garde96@gmail.com>
2021-07-20 00:24:51 +0530

    Naming Server Initial Commit

---
+++ naming-server/src/test/java/com/github/adityagarde/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adityagarde.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
