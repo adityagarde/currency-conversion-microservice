2eeec48248b7199292827e9c9e66725fef06911e
adityagarde <aditya.garde96@gmail.com>
2021-07-20 15:59:20 +0530

    Initial Commit

---
+++ limits-service/src/test/java/com/github/adityagarde/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adityagarde.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
