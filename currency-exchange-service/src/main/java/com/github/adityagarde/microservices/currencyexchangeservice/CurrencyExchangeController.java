ea2acaedc43c7c5d7e7a79e7671bc71924d6d379
adityagarde <aditya.garde96@gmail.com>
2021-07-28 01:07:06 +0530

    Enabling Distributed Tracing with Zipkin and Sleuth

--- currency-exchange-service/src/main/java/com/github/adityagarde/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/github/adityagarde/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.github.adityagarde.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -17,6 +21,9 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange fetchExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+        logger.info("Inside call of fetchExchangeValue - from == " + from + " to " + to);
+
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
             throw new RuntimeException("No data found for from == " + from + " and to == " + to);
